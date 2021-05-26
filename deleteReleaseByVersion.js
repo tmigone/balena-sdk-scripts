334ce7e7bca469f4b1c4b9f5544c2a5ab21cb415
Tomás Migone <tomas@balena.io>
2021-05-26 15:43:15 -0300

    Update deleteReleaseByVersion
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- deleteReleaseByVersion.js
+++ deleteReleaseByVersion.js
@@ -1,43 +1,42 @@
 const { getSdk } = require('balena-sdk')
 const fs = require('fs')
+const yesno = require('yesno')
 
 const BALENA_API_TOKEN = fs.readFileSync('token', 'utf-8')
 const balena = getSdk({
   apiUrl: 'https://api.balena-cloud.com/'
 })
 
-const appId = 1677085
-const version = '2.0.0'
+const appId = 1577458
+const version = '3.5.5'
 
 async function run () {
   try {
     await balena.auth.loginWithToken(BALENA_API_TOKEN)
 
     const result = await balena.pine.get({
-      resource: 'application',
+      resource: 'release',
       options: {
-        $expand: {
-          owns__public_device: {
-            $select: ['was_recently_online']
-          }
-        },
         $filter: {
-          is_public: true,
-          is_discoverable: true,
-          is_host: false,
-          is_of__class: 'fleet',
-          is_stored_at__repository_url: { $ne: null }
+          belongs_to__application: appId,
+          release_version: version
         }
       }
     })
-    console.log(result.map(r => r.id))
+    console.log(`Relase IDs: ${result.map(r => r.id)}`)
 
-    for (const id of result.map(r => r.id)) {
-      const deletedId = await balena.pine.delete({
-        resource: 'release',
-        id: id
-      })
-      console.log(`Deleted release with id ${id}: ${deletedId}`)
+    const ok = await yesno({
+      question: 'Are you sure you want to continue?'
+    })
+
+    if (ok) {
+      for (const id of result.map(r => r.id)) {
+        const deletedId = await balena.pine.delete({
+          resource: 'release',
+          id: id
+        })
+        console.log(`Deleted release with id ${id}: ${deletedId}`)
+      }
     }
   } catch (error) {
     console.log(error)
